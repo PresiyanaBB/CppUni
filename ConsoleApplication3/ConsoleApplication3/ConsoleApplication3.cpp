8e6ab2802b1b48cc24cec1bea330795feadc8881
Presiyana Borisova <84325049+PresiyanaBB@users.noreply.github.com>
2022-10-05 18:58:06 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double levove;
+    cin >> levove;
+    cout << "euro: " << levove * 0.51 << "\ndolar: " << levove * 0.5;
+
+    return 0;
+}
+
+//Да се напише програма, която чете от конзолата дадена сума в лева и ги превръща в долари и в евра.
