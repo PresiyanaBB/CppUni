8e4681dca2c0e8439aaaad2add79a444699e8b11
Presiyana Borisova <84325049+PresiyanaBB@users.noreply.github.com>
2022-10-05 19:41:10 +0300

    Add files via upload

---
+++ ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int apples;
+    int pears;
+    int bananas;
+
+    cout << "Apples: ";
+    cin >> apples;
+    cout << "Pears: ";
+    cin >> pears;
+    cout << "Bananas: ";
+    cin >> bananas;
+
+    cout << "Pesho, don't forget to buy " << apples << " apples, " << pears << " pears and " << bananas << " bananas!";
+
+    return 0;
+}
+
+/*
+Apples: 5
+Pears: 6
+Bananas: 3
+Èçõîä:
+
+Pesho, don’t forget to buy 5 apples, 6 pears and 3 bananas!*/
