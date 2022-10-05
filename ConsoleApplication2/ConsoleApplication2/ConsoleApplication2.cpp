b73bc05907ac28decbfbc61b0014f7752519b1a0
Presiyana Borisova <84325049+PresiyanaBB@users.noreply.github.com>
2022-10-05 18:53:14 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Oh what\na happy day!\nOh yes,\nwhat a happy day!";
+
+    return 0;
+}
+
+/*
+Oh what
+a happy day!
+Oh yes,
+what a happy day!*/
