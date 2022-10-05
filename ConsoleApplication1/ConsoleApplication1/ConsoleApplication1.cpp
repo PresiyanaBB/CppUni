c25f78aea3d8ac2e485ce5bb011c24da35ea7cfb
Presiyana Borisova <84325049+PresiyanaBB@users.noreply.github.com>
2022-10-05 18:50:58 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const float a = 5.4;
+    const float b = 7.9;
+
+    cout << "Square with a=" << a << " and b="<< b <<"\nP: " << 2*(a+b) << "\nS: " << a * b;
+
+    return 0;
+}
+
+//Да се напише програма, която изчислява периметъра и лицето на правоъгълник със страни 5,4 и 7,9.
